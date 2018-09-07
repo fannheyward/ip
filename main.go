169e464e4d889324520b777725346e0ffaf292c3
Heyward Fann <heyward.fan@kikatech.com>
2018-09-07 16:03:04 +0800

    1

---
+++ main.go
@@ -0,0 +1,70 @@
+package main
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"os/exec"
+	"regexp"
+	"sync"
+)
+
+func parse(input string) ([]string, error) {
+	ips := make([]string, 0)
+	r, err := regexp.Compile("^([0-9]{1,3}\\.){3}[0-9]{1,3}$")
+	if err != nil {
+		return nil, err
+	}
+
+	if r.MatchString(input) {
+		ips = append(ips, r.FindString(input))
+		return ips, nil
+	}
+
+	nips, err := net.LookupIP(input)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, v := range nips {
+		ips = append(ips, v.String())
+	}
+
+	return ips, nil
+}
+
+func fetchIPCN(ip string) {
+	_url := "http://ip.cn/" + ip
+	cmd := exec.Command("curl", "-s", "-L", _url)
+	out, err := cmd.CombinedOutput()
+	if err != nil {
+		return
+	}
+
+	fmt.Print(string(out))
+}
+
+func main() {
+	args := os.Args
+	if len(args) != 2 {
+		fmt.Print("one arg only")
+		os.Exit(1)
+	}
+
+	ips, err := parse(args[1])
+	if err != nil {
+		fmt.Print("no IP found.")
+		os.Exit(1)
+	}
+
+	wg := sync.WaitGroup{}
+	for _, v := range ips {
+		wg.Add(1)
+		go func(ip string) {
+			defer wg.Done()
+			fetchIPCN(ip)
+		}(v)
+	}
+
+	wg.Wait()
+}
---
+++ main_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"testing"
+)
+
+func TestParser(t *testing.T) {
+	ips, err := parse("127.0.0.1")
+	if err != nil {
+		t.Fatal(err.Error())
+	}
+
+	if len(ips) != 1 {
+		t.Fatal("length error")
+	}
+
+	if ips[0] == "127.0.0.1" {
+		t.Log("127.0.0.1 OK")
+	}
+}
+
+func TestIP(t *testing.T) {
+	fetchIPCN("127.0.0.1")
+}
