31a642d92281294c54d553fee41b05f886ba813e
Heyward Fann <fannheyward@gmail.com>
2021-04-23 14:40:36 +0800

    update

--- main_test.go
+++ main_test.go
@@ -1,30 +1,9 @@
 package main
 
 import (
-	"net"
 	"testing"
 )
 
-func TestParser(t *testing.T) {
-	ips := []string{
-		"127.0.0.1",
-		"8.8.8.8",
-		"2001:4860:4860::8888",
-		"192.168.1",
-	}
-
-	for _, v := range ips {
-		ip := net.ParseIP(v)
-		if ip == nil {
-			t.Logf("%s is NOT valid", v)
-		}
-
-		if ip.String() == v {
-			t.Logf("%s is valid", v)
-		}
-	}
-}
-
 func TestIP(t *testing.T) {
-	fetchIP("127.0.0.1")
+	fetchIP(hostIP{domain: "LocalHost", ip: "127.0.0.1"})
 }
